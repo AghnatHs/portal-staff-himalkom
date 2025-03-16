afeae2a68926903c879e7e40a01ab202b56e1f5a
AghnatHs <aghnathasya@gmail.com>
2025-03-16 22:54:42 +0700

    Add auto-generated password after creating users

--- app/Filament/Resources/UserResource/Pages/CreateUser.php
+++ app/Filament/Resources/UserResource/Pages/CreateUser.php
@@ -2,11 +2,20 @@
 
 namespace App\Filament\Resources\UserResource\Pages;
 
-use App\Filament\Resources\UserResource;
 use Filament\Actions;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
+use App\Filament\Resources\UserResource;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateUser extends CreateRecord
 {
     protected static string $resource = UserResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $randomPassword = Str::random(12);
+        $data['password'] = Hash::make($randomPassword);
+        return $data;
+    }
 }
