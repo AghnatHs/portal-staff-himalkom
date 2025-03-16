86147347e4becdfd589c9894ec1ee7f52116a945
AghnatHs <aghnathasya@gmail.com>
2025-03-16 20:56:18 +0700

    Add Department setup

---
+++ app/Models/Department.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Department extends Model
+{
+    use HasUlids, SoftDeletes;
+    public $incrementing = false;
+    protected $keyType = 'string';
+
+    protected $fillable = [
+        'name',
+        'abbreviation',
+        'slug',
+        'description',
+    ];
+
+    public function users(): HasMany
+    {
+        return $this->hasMany(User::class);
+    }
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(function ($model) {
+            if (!$model->id) {
+                $model->id = Str::ulid()->toBase32(); // Generate ULID
+            }
+        });
+    }
+}
