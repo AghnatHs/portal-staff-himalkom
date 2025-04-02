d435cc4e076edf2b5d941ad8cac0fdd1017a9dcd
AghnatHs <aghnathasya@gmail.com>
2025-04-02 08:51:24 +0700

    Change back button (breadcrumbs) hover color to text-black:

--- resources/views/dashboard/modview/show-department.blade.php
+++ resources/views/dashboard/modview/show-department.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <div class="flex items-center gap-4">
-            <a href="{{ route('dashboard.modview.department.index') }}" class="text-gray-600 hover:text-gray-900">
+            <a href="{{ route('dashboard.modview.department.index') }}" class="text-gray-600 hover:text-black">
                 <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 inline-block" fill="none" viewBox="0 0 24 24"
                     stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" />
--- resources/views/dashboard/modview/show-workprogram.blade.php
+++ resources/views/dashboard/modview/show-workprogram.blade.php
@@ -2,7 +2,7 @@
     <x-slot name="header">
         <div class="flex items-center gap-4">
             <a href="{{ route('dashboard.modview.department.show', ['department' => $workProgram->department]) }}"
-                class="text-gray-600 hover:text-gray-900">
+                class="text-gray-600 hover:text-black">
                 <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 inline-block" fill="none" viewBox="0 0 24 24"
                     stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" />
