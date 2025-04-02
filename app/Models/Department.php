274a99b05603388575409da28ff1f4cf354d3c75
AghnatHs <aghnathasya@gmail.com>
2025-04-02 09:21:59 +0700

    Update Department laravel_ide_helper

--- app/Models/Department.php
+++ app/Models/Department.php
@@ -19,6 +19,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
  * @property \Illuminate\Support\Carbon|null $deleted_at
+ * @property-read mixed $managing_director
  * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\User> $users
  * @property-read int|null $users_count
  * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\WorkProgram> $workPrograms
