9bc0cecf0a076970c73c0f6fc64c1bff2e48708a
unknown <anargya115@gmail.com>
2025-04-05 21:28:08 +0800

    feat: update user interface index work programs

--- resources/views/dashboard/workprograms/index.blade.php
+++ resources/views/dashboard/workprograms/index.blade.php
@@ -53,50 +53,67 @@
             </script>
         @endif
 
-
         <div class="flex justify-end mb-3">
             <a href="{{ route('dashboard.workProgram.create', ['department' => $department]) }}"
-                class="mr-2 lg:mr-0 bg-green-600 text-white px-2 py-1 md:px-4 md:py-2 rounded-lg shadow hover:bg-green-800 transition text-md md:text-lg">
+                class="mr-2 lg:mr-0 
+                       bg-[#111B5A] 
+                       text-white 
+                       px-2 py-1 md:px-3 md:py-2 
+                       rounded-lg 
+                       shadow-md 
+                       hover:bg-[#14267B] 
+                       transition duration-200 
+                       text-[12px] md:text-sm
+                       font-semibold 
+                       tracking-wide 
+                       flex items-center gap-2">
+                <span class="text-lg md:text-xl">+</span>
                 Tambah Program Kerja
             </a>
         </div>
 
+
+
         <div class=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 lg:gap-4">
             @forelse ($department->workPrograms as $workProgram)
                 <div
-                    class="mx-2 lg:mx-0 bg-white shadow-md rounded-[10px] p-4 border border-gray-200 hover:shadow-lg transition flex flex-col">
-                    <h2 class="uppercase text-lg md:text-xl font-bold text-gray-900">{{ $workProgram->name }}</h2>
-                    <p class="text-[10px] mb-2">id: {{ $workProgram->id }}</p>
+                    class="flex flex-col gap-1 lg:gap-3 relative mx-2 lg:mx-0 bg-white/90 border border-[#111B5A]/30 hover:border-[#14267B]/40 shadow-inner hover:shadow-md rounded-xl p-5 transition duration-200 transform backdrop-blur-sm">
 
-                    <div class="my-1 md:my-2">
-                        <h3 class="text-sm font-semibold text-gray-700">Description</h3>
-                        <p class="text-gray-600 text-sm my-1 md:my-2 break-words">
-                            {{ Str::limit($workProgram->description, 100, '...') }}</p>
-                        <hr class="border-t border-gray-200 my-2">
+                    <p class="absolute bottom-3 right-4 text-[10px] text-gray-400 italic">
+                        ID: {{ $workProgram->id }}
+                    </p>
+
+                    <h2 class="uppercase text-lg md:text-xl font-bold text-[#111B5A] tracking-wide">
+                        {{ $workProgram->name }}
+                    </h2>
+
+                    <div>
+                        <h3 class="text-[14px] md:text-sm font-semibold text-[#14267B]">Description</h3>
+                        <p class="text-gray-600 text-[12px] md:text-sm mt-1 break-words">
+                            {{ Str::limit($workProgram->description, 60, '...') }}
+                        </p>
                     </div>
 
-                    <div class="my-1 md:my-2">
-                        <h3 class="text-sm font-semibold text-gray-700">Timeline</h3>
-                        @php
-                            $diffInDays = \Carbon\Carbon::parse($workProgram->start_at)->diffInDays(
-                                \Carbon\Carbon::parse($workProgram->finished_at),
-                            );
-                        @endphp
-                        <p class="text-gray-500 text-[12px] my-1">{{ $workProgram->timeline_range_text }}</p>
-                        <hr class="border-t border-gray-200 my-2">
+                    <div>
+                        <h3 class="text-[14px] md:text-sm font-semibold text-[#14267B]">Timeline</h3>
+                        <p class="mt-1 text-[12px] md:text-sm text-gray-500">{{ $workProgram->timeline_range_text }}</p>
+                    </div>
+                    <div>
+                        <h3 class="font-semibold text-[14px] md:text-sm text-[#14267B]">Last Updated</h3>
+                        <p class="mt-1 text-[12px] md:text-sm text-gray-500">
+                            {{ \Carbon\Carbon::parse($workProgram->created_at)->diffForHumans() }}
+                        </p>
                     </div>
 
-                    <div class="my-1 md:my-2">
-                        <h3 class="text-sm font-semibold text-gray-700">Last Updated</h3>
-                        <p class="text-gray-500 text-[12px]">
-                            {{ \Carbon\Carbon::parse($workProgram->created_at)->diffForHumans() }}</p>
-                        <hr class="border-t border-gray-200 my-2">
-                    </div>
-
-                    <div class="mt-1 md:mt-2">
+                    <div class="mt-2">
                         <a href="{{ route('dashboard.workProgram.detail', ['workProgram' => $workProgram, 'department' => $department]) }}"
-                            class="inline-block text-blue-500 text-xs font-semibold hover:underline">
-                            Read More →
+                            class="inline-flex items-center gap-1 text-white bg-[#111B5A] hover:bg-[#14267B] transition px-3 py-1.5 text-xs font-medium rounded-full">
+                            Read More
+                            <svg xmlns="http://www.w3.org/2000/svg" class="w-3 h-3" fill="none" viewBox="0 0 24 24"
+                                stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M9 5l7 7-7 7" />
+                            </svg>
                         </a>
                     </div>
                 </div>
